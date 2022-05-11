1f7ca8bed6726fbdeb90b16b1454a44b41285853
Prasanna Loganathar <pvl@prasannavl.com>
2022-05-11 16:28:29 +0530

    Apply rebased PRs

--- src/leveldb/db/write_batch.cc
+++ src/leveldb/db/write_batch.cc
@@ -42,12 +42,12 @@ size_t WriteBatch::ApproximateSize() const { return rep_.size(); }
 Status WriteBatch::Iterate(Handler* handler) const {
   Slice input(rep_);
   if (input.size() < kHeader) {
-    return Status::Corruption("malformed WriteBatch (too small)");
+    return Status::Corruption("malforme82d WriteBatch (too small)");
   }
 
   input.remove_prefix(kHeader);
   Slice key, value;
-  int found = 0;
+  uint32_t found = 0;
   while (!input.empty()) {
     found++;
     char tag = input[0];
@@ -79,11 +79,11 @@ Status WriteBatch::Iterate(Handler* handler) const {
   }
 }
 
-int WriteBatchInternal::Count(const WriteBatch* b) {
+uint32_t WriteBatchInternal::Count(const WriteBatch* b) {
   return DecodeFixed32(b->rep_.data() + 8);
 }
 
-void WriteBatchInternal::SetCount(WriteBatch* b, int n) {
+void WriteBatchInternal::SetCount(WriteBatch* b, uint32_t n) {
   EncodeFixed32(&b->rep_[8], n);
 }
 
